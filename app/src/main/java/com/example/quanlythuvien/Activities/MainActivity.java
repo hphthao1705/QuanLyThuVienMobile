ce2162664b150ae0db3f03fe45c7f38d1015fec0
nguynvanky <k.nguyn21@gmail.com>
2023-10-06 23:45:47 +0700

    init

---
+++ app/src/main/java/com/example/quanlythuvien/Activities/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.quanlythuvien.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.quanlythuvien.Fragments.CartBookFragment;
+import com.example.quanlythuvien.Fragments.FavouriteFragment;
+import com.example.quanlythuvien.Fragments.HomeFragment;
+import com.example.quanlythuvien.Fragments.InformationFragment;
+import com.example.quanlythuvien.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.example.quanlythuvien.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment current = new HomeFragment();
+    Integer current_page = R.id.page_home;
+
+    Integer PageHome  = R.id.page_home;
+    Integer PageFavourite  = R.id.page_favourite;
+    Integer PageSearch  = R.id.page_search;
+    Integer PageCartBook  = R.id.page_cart_book;
+    Integer PageInformation  = R.id.page_information;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        // Set Home selected
+        Intent i = getIntent();
+        loadFragment(current);
+        bottomNavigationView.setSelectedItemId(current_page);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                if (item.getItemId() == R.id.page_home)
+                {
+                    fragment = new HomeFragment();
+                }
+                else  if (item.getItemId() == R.id.page_favourite)
+                {
+                    fragment = new FavouriteFragment();
+                }
+                else  if (item.getItemId() == R.id.page_search)
+                {
+                    fragment = new SearchFragment();
+                }
+                else  if (item.getItemId() == R.id.page_cart_book)
+                {
+                    fragment = new CartBookFragment();
+                }
+                else  if (item.getItemId() == R.id.page_information)
+                {
+                    fragment = new InformationFragment();
+                }
+                if(fragment == null)
+                {
+
+                    return false;
+                }
+                   loadFragment(fragment);
+                return true;
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        //transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/quanlythuvien/Adapters/FavouriteAdapter.java
@@ -0,0 +1,94 @@
+package com.example.quanlythuvien.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.quanlythuvien.Dtos.DtoFavourite;
+import com.example.quanlythuvien.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class FavouriteAdapter extends RecyclerView.Adapter<FavouriteAdapter.FavouriteViewHolder>  {
+    List<DtoFavourite> list;
+    Context context;
+    public  FavouriteAdapter(Context t ){
+        context = t;
+    }
+
+    public void SetData(List<DtoFavourite> l)
+    {
+        list= l;
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public FavouriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_favourite_book,parent,false);
+        return new FavouriteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavouriteViewHolder holder, int position) {
+        DtoFavourite dtoFavourite = list.get(position);
+        if(dtoFavourite == null)
+        {
+            return;
+        }
+        holder.nameOfBook.setText(dtoFavourite.TenSach);
+        holder.imgBook.setImageResource(R.drawable.conan);
+
+        holder.authorOfBook.setText(dtoFavourite.TacGia);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        holder.btnRent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if(list != null)
+            return list.size();
+        return 0;
+    }
+
+    public class FavouriteViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imgBook;
+        private TextView nameOfBook;
+        private TextView authorOfBook;
+        private Button btnRent;
+
+        public FavouriteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            imgBook = itemView.findViewById(R.id.img_favourite_book);
+            nameOfBook = itemView.findViewById(R.id.txt_tensach_favourite);
+            authorOfBook = itemView.findViewById(R.id.txt_tacgia_favourite);
+            btnRent = itemView.findViewById(R.id.btn_muonngay_favourite);
+        }
+    }
+}
