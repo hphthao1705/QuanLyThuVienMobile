69719a9c6d7e3f55c3790de7bb89694a713d0bb0
nguynvanky <k.nguyn21@gmail.com>
2023-10-29 22:43:00 +0700

    add fragment detail book

--- app/src/main/java/com/example/qlthuvien/view/activities/DetailsBookActivity.java
+++ app/src/main/java/com/example/qlthuvien/view/activities/DetailsBookActivity.java
@@ -2,7 +2,11 @@ package com.example.qlthuvien.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.qlthuvien.R;
 
@@ -11,6 +15,14 @@ public class DetailsBookActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_details_book);
+        ImageButton btn_back =(ImageButton) findViewById(R.id.btn_details_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
